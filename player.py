eb209a426a4270d77ee23bfc5fb7a4e74c0a0783
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-15 10:47:47 -0300

    feat: idle and move animation

--- player.py
+++ player.py
@@ -1,13 +1,18 @@
 import pygame
 from settings import *
+from support import *
+from re import search
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group) -> None:
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # general setup
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         # movement attributes
@@ -15,22 +20,51 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [],'right': [],
+                            'right_idle': [], 'left_idle': [],'up_idle': [],'down_idle': [],
+                            'right_hoe': [],'left_hoe': [],'up_hoe': [], 'down_hoe': [],
+                            'right_water': [],'left_water': [],'up_water': [],'down_water': []}
+
+        for animation in self.animations.keys():
+            full_path = 'graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
+
+    def get_status(self):
+        # if the player is not moving:
+        if self.direction.magnitude() == 0:
+            # add _idle to the status
+            if not search('_idle', self.status):
+                self.status += '_idle'
+        
 
     def move(self, dt):
 
@@ -47,5 +81,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.centery = self.pos.y
     
     def update(self, dt):
+        self.get_status()
         self.input()
         self.move(dt)
+        self.animate(dt)
