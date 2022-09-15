e23e87469957991a60465cd87fc094f1e495d253
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-15 15:58:47 -0300

    feat: axe func, tree health, apples and tree stomp

--- sprites.py
+++ sprites.py
@@ -1,5 +1,8 @@
+from secrets import choice
 import pygame
 from settings import *
+from random import randint, random
+from timer import Timer
 
 
 class Generic(pygame.sprite.Sprite):
@@ -46,3 +49,46 @@ class WildFlower(Generic):
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name) -> None:
         super().__init__(pos, surf, groups)
+
+        # tree atrributes
+        self.health = 5
+        self.alive = True
+        stump_path = 'graphics/stumps'
+        self.stump_surf = pygame.image.load(f'{stump_path}/{"small" if name == "Small" else "Large"}.png').convert_alpha()
+        self.invul_timer = Timer(200)
+
+        # apples
+        self.apple_surf = pygame.image.load('graphics/fruit/apple.png')
+        self.apple_pos = APPLE_POS[name]
+        self.apple_sprites = pygame.sprite.Group()
+        self.create_fruit()
+
+    def damage(self):
+        # damaging the tree
+        self.health -= 1
+
+        # remove an apple
+        if len(self.apple_sprites.sprites()) > 0:
+            random_apple = choice(self.apple_sprites.sprites())
+            random_apple.kill()
+
+    def create_fruit(self):
+        for pos in self.apple_pos:
+            if randint(0,10) < 2:
+                x = pos[0] + self.rect.left
+                y = pos[1] + self.rect.top
+                Generic(pos = (x, y),
+                surf = self.apple_surf,
+                groups = [self.apple_sprites, self.groups()[0]],
+                z = LAYERS['fruit'])
+    
+    def check_death(self):
+        if self.health <= 0:
+            self.image = self.stump_surf
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
+            self.alive = False
+    
+    def update(self, dt) -> None:
+        if self.alive:
+            self.check_death()
