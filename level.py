44b869f8217424547e8ccdc27fd1dfd0dab46f18
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-19 11:22:05 -0300

    feat: soil.py implement farm mechanics

--- level.py
+++ level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Interaction, Tree, Water, WildFlower
 from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
+from soil import SoilLayer
 
 class Level:
     def __init__(self) -> None:
@@ -19,9 +20,11 @@ class Level:
         self.tree_sprites = pygame.sprite.Group()
         self.interaction_sprites = pygame.sprite.Group()
 
+        self.soil_layer = SoilLayer(self.all_sprites)
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
+
 
     def setup(self):
         tmx_data = load_pygame('data/map.tmx')
@@ -70,7 +73,8 @@ class Level:
                                                     group = self.all_sprites,
                                                     collision_sprites = self.collision_sprites,
                                                     tree_sprites = self.tree_sprites,
-                                                    interaction = self.interaction_sprites
+                                                    interaction = self.interaction_sprites,
+                                                    soil_layer = self.soil_layer
                                                     )
                             
             if obj.name == 'Bed':
